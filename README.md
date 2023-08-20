44ceb9d8cf5beb1454c08084c7ce328de78ec831
Your Name <william.descamps@gmail.com>
2023-08-20 19:05:53 +0200

    First push

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# portfolio
+# Source
+Original template from : 
+<a href="https://raw.githubusercontent.com/crearo/portfolio">crearo_github</a>
+
+# His Website
+<a href="https://www.rish.space">rish.space</a>
---
+++ static/js/home.js
@@ -0,0 +1,26 @@
+$(function() {
+  /* NOTE: hard-refresh the browser once you've updated this */
+  $(".typed").typed({
+    strings: [
+      "stat about_me<br/>" +
+      "><span class='caret'>$</span> Job: Cognitive/Musical/Sports/Quirky  Data Explorer<br/> ^100" +
+      "><span class='caret'>$</span> Skills: Asking question. Creating Links. Finding fun answers<br/> ^100" +
+      "><span class='caret'>$</span> Interests: Neuropsychology, Space exploration, Bike-packing, Surfing, Volleyball, Coaching...<br/> ^300" +
+      "><span class='caret'>$</span> Let's switch roles, how about you?? <br/> ^300"
+    ],
+    showCursor: true,
+    cursorChar: '_',
+    autoInsertCss: true,
+    typeSpeed: 0.001,
+    startDelay: 50,
+    loop: false,
+    showCursor: false,
+    onStart: $('.message form').hide(),
+    onStop: $('.message form').show(),
+    onTypingResumed: $('.message form').hide(),
+    onTypingPaused: $('.message form').show(),
+    onComplete: $('.message form').show(),
+    onStringTyped: function(pos, self) {$('.message form').show();},
+  });
+  $('.message form').hide()
+});
